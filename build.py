0c474f21a0657996daa06cb616ef04115c01ed88
Maxx100 <rezonyou@gmail.com>
2024-08-08 22:54:03 +0300

    init

---
+++ build.py
@@ -0,0 +1,27 @@
+import os
+
+NAME = "mmc"
+
+
+def rdir(path):
+	for i in os.listdir(path):
+		if os.path.isfile(f"{path}/{i}"):
+			os.remove(f"{path}/{i}")
+		else:
+			rdir(f"{path}/{i}")
+	os.rmdir(path)
+
+
+if os.path.exists("dist"):
+	rdir("dist")
+os.system(
+	"pyinstaller "
+	"--onefile "
+	f"-n {NAME} "
+	"--add-data \"data:data\" "
+	"main.py"
+)
+if os.path.exists(f"{NAME}.spec"):
+	os.remove(f"{NAME}.spec")
+if os.path.exists("build"):
+	rdir("build")
